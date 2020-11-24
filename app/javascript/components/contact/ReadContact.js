5784dbb83ebd09f4eae3d7d9416984ea2635ed56
Diego Plancarte <dplancarte7@gmail.com>
2020-11-23 21:30:11 -0800

    Created table to show message information.

--- app/javascript/components/contact/ReadContact.js
+++ app/javascript/components/contact/ReadContact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap'
+import { Container, Table } from 'react-bootstrap'
 import useRead from '../hooks/useRead'
 
 const ReadContact = (props) => {
@@ -16,18 +16,34 @@ const ReadContact = (props) => {
 
   return ( 
     <React.Fragment>
-      {contacts.map((v,i) => {
-        return(
-        <div key={i}>
-          <p>{v.name}</p>
-          <p>{v.title}</p>
-          <p>{v.body}</p>
-          <p>{v.email}</p>
-          <p>{v.phone}</p>
-          <p>{v.status}</p>
-        </div>
-        )
-      })}
+      <Table responsive="sm" striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((v,i) => {
+            return(
+            <tr key={i}>
+              <td>{v.id}</td>
+              <td>{v.name}</td>
+              <td>{v.title}</td>
+              <td>{v.body}</td>
+              <td>{v.email}</td>
+              <td>{v.phone}</td>
+              <td>{v.status}</td>
+            </tr>
+            )
+          })}
+        </tbody>
+      </Table>
     </React.Fragment>
   );
 }
