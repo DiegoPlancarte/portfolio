4e3cda36d0f3107922104fe7eb5817e7e6fb6041
Diego Plancarte <dplancarte7@gmail.com>
2020-11-23 21:19:19 -0800

    Added loading screen while information is being retreived

--- app/javascript/components/contact/ReadContact.js
+++ app/javascript/components/contact/ReadContact.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
 import useRead from '../hooks/useRead'
 
 const ReadContact = (props) => {
 
   const [contacts, setContacts, contactsLoading, contactErrors ] = useRead('contacts')
+
+  if (contactsLoading) {
+    return <Container>
+              <div >
+                <h1 className="text-primary">Loading...</h1>
+              </div>
+          </Container>
+  }
 
   return ( 
     <React.Fragment>
@@ -11,10 +20,10 @@ const ReadContact = (props) => {
         return(
         <div key={i}>
           <p>{v.name}</p>
-          <p>{v.email}</p>
-          <p>{v.phone}</p>
           <p>{v.title}</p>
           <p>{v.body}</p>
+          <p>{v.email}</p>
+          <p>{v.phone}</p>
           <p>{v.status}</p>
         </div>
         )
