a57653a8d5d50a11fc5901ac935e84f7a3f60ede
Diego Plancarte <dplancarte7@gmail.com>
2020-11-24 20:59:43 -0800

    Add icons for Delete, Mark as Read, Mark as Unread to details page

--- app/javascript/components/message/MessageInfo.js
+++ app/javascript/components/message/MessageInfo.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Container } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
+import { IconContext} from 'react-icons';
+import { FaRegTrashAlt, FaEnvelope, FaEnvelopeOpen } from 'react-icons/fa'
 
 import useRead from '../hooks/useRead'
+import useUpdate from '../hooks/useUpdate'
+import useDelete from '../hooks/useDelete'
 
 const MessageInfo = (props) => {
 
   const [ message, setMessage, messageLoading, messageError ] = useRead(`contacts/${props.match.params.id}`)
-  const [status, statusLoading, statusErrors ] = useUpdate(`contacts/${messages.id}`)
+  const [status, statusLoading, statusErrors ] = useUpdate(`contacts/${message.id}`)
   const [ deleteMessage ] = useDelete(`contacts/${props.match.params.id}`, props, 'messages')
 
   if (messageLoading) {
@@ -17,9 +21,59 @@ const MessageInfo = (props) => {
           </Container>
   }
 
+  const handleDelete = () => {
+    return (
+      <IconContext.Provider value={{ size: "2em", color: 'red' }}>
+        <div className="p-3 icon">
+          <FaRegTrashAlt onClick={ deleteMessage }/>
+        </div>
+      </IconContext.Provider>
+      )
+  }
+
+  const handleRead = () => {
+    if(message.status === 'Read'){
+    return (
+      <IconContext.Provider value={{ size: "2em", color: 'blue' }}>
+        <div className="p-3 icon">
+          <FaEnvelope onClick={ deleteMessage }/>
+        </div>
+      </IconContext.Provider>
+      )}
+      return (
+        <IconContext.Provider value={{ size: "2em", color: 'grey' }}>
+          <div className="p-3 icon">
+            <FaEnvelope onClick={ deleteMessage }/>
+          </div>
+        </IconContext.Provider>
+        )
+  }
+
+  const handleUnread = () => {
+    if(message.status === 'Unread'){
+    return (
+      <IconContext.Provider value={{ size: "2em", color: 'blue' }}>
+        <div className="p-3 icon">
+          <FaEnvelopeOpen onClick={ deleteMessage }/>
+        </div>
+      </IconContext.Provider>
+      )}
+      return (
+        <IconContext.Provider value={{ size: "2em", color: 'grey' }}>
+          <div className="p-3 icon">
+            <FaEnvelopeOpen onClick={ deleteMessage }/>
+          </div>
+        </IconContext.Provider>
+        )
+  }
+
+
   return ( 
     <React.Fragment>
       <Container>
+        { handleDelete() }
+        { handleRead() }
+        { handleUnread() }
         <p>{message.title}</p>
         <p>{message.body}</p>
         <p>{message.name}</p>
