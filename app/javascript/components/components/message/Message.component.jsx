53b9c4174c00d6466c7f87c0cf20c77099cc28db
Diego Plancarte <dplancarte7@gmail.com>
2020-12-22 23:55:56 -0800

    Edit message component

--- app/javascript/components/components/message/Message.component.jsx
+++ app/javascript/components/components/message/Message.component.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import './message.stles.scss'
+import useRead from '../hooks/useRead'
+import useUpdate from '../hooks/useUpdate'
+import useDelete from '../hooks/useDelete'
+import Loading from '../../Lo'
 
 const Message = () => {
+
+  const [ message, setMessage, messageLoading, messageError ] = useRead(`contacts/${props.match.params.id}`)
+  const [status, statusLoading, statusErrors ] = useUpdate(`contacts/${message.id}`)
+  const [ deleteMessage ] = useDelete(`contacts/${props.match.params.id}`, props, 'messages')
+
+  if (messageLoading) {
+    return (<Loading/>)
+  }
+
   return (
     <div>
       
